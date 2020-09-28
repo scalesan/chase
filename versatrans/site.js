7bec356d5787929ff7f742e1248af44f78503b51
Anthony Scales <anthony,scalesii@gmail.com>
2020-09-28 07:44:34 -0400

    Changed JavaScript file to include URL and altered HTML

--- versatrans/site.js
+++ versatrans/site.js
@@ -1,3 +1,9 @@
-document.getElementById('submitButton').onclick = function(e){
-    alert('Submit clicked');
-  }
+document.getElementById("submitButton").onclick = function (e) {
+  let urlparam = document.getElementById("inputValue").value;
+  let response = fetch(
+    "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=" +
+      urlparam +
+      "&apikey=5KZBIG65DG43J3MN"
+  );
+  console.log(response);
+};
