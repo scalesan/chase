20c4d3ac82668144479bac4a08063b75f0693450
Anthony Scales <anthony,scalesii@gmail.com>
2020-09-29 15:56:06 -0400

    Didnt mean to do this commit
    This reverts commit d87cf97f88707f8393f7833b74847afb7130ccc5.

--- versatrans/site.js
+++ versatrans/site.js
@@ -1,13 +1,13 @@
 document.getElementById("submitButton").onclick = function (e) {
-    let urlparam = document.getElementById("inputValue").value;
-    fetch(
-      "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=" +
-        urlparam +
-        "&apikey=5KZBIG65DG43J3MN"
-    )
-      .then((res) => res.json())
-      .then((JSONOutput) => {
-        console.log("Output: ", JSONOutput);
-      })
-      .catch((err) => console.error(err));
-  };
+  let urlparam = document.getElementById("inputValue").value;
+  fetch(
+    "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=" +
+      urlparam +
+      "&apikey=5KZBIG65DG43J3MN"
+  )
+    .then((res) => res.json())
+    .then((JSONOutput) => {
+      console.log("Output: ", JSONOutput);
+    })
+    .catch((err) => console.error(err));
+};
