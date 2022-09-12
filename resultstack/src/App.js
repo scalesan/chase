943f9b132cf7dfbb6225f974f3f2fb7012c13630
Anthony Scales <anthony,scalesii@gmail.com>
2022-09-12 10:20:56 -0400

    The latest version of file

--- resultstack/src/App.js
+++ resultstack/src/App.js
@@ -1,13 +1,42 @@
 import axios from 'axios';
+import { useState } from 'react';
 
 const App = () => {
-  axios.get('')
-  return (
+  const [name, setName] = useState({
+    name:""
+  })
 
+const handleNameEnteredChange = (e) => {
+  axios.get("https://docs.github.com/en/free-pro-team@latest/rest)")
+  .then((response) => {
+    const users = response.filter(
+      e => response.name.includes(e.target.value)).map(filteredName => (
+        <li>
+          {e.target.value}
+        </li>
+      ))
+    }
+  )}
+  
+ 
+  return (
     <div>
-      Hello this is a test app
+      <form>
+        Please begin typing the name: 
+        <input 
+          type="text" 
+          onChange={handleNameEnteredChange}
+          placeholder = "Name"
+          />
+      </form>
+      <ul>
+        <li>
+        </li>
+      </ul>
     </div>
-  );
+  )
 }
+
+
 
 export default App;
